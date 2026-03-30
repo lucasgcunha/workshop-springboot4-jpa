fdc1001b6a5b80b1f112ac7336cecf43ff153139
Lucas Granier <lucgcunha@gmail.com>
2026-03-29 22:25:53 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,13 +1,19 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = -6312538286007305803L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
